8540ae828b53192214885b880f6e5e3300e8a0c6
Yuxuan Chen <yuxuanchen@stmarksschool.org>
2019-01-27 15:46:17 -0500

    update view to support radical

--- jiezi/learning/views.py
+++ jiezi/learning/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
-from learning.models import Character
+from learning.models import Character, Radical
 
 
 def index(request):
@@ -18,4 +17,16 @@ def index(request):
 
 # @login_required
 def learning_character(request, character_pk):
-    return render(request, 'learning/learning_interface.html', {'character':Character.objects.get(pk=character_pk)})
+    character = Character.objects.get(pk=character_pk)
+    dict = {'character':character , 'radical_1':Radical.objects.get(pk=character.mnemonic_1)}
+    try:
+        radical_2=Radical.objects.get(pk=character.mnemonic_2)
+        dict['radical_2'] = radical_2
+    except:
+        dict['radical_2'] = None
+    try:
+        radical_3=Radical.objects.get(pk=character.mnemonic_3)
+        dict['radical_3'] = radical_3
+    except:
+        dict['radical_3'] = None
+    return render(request, 'learning/learning_interface.html', dict)
