d8a94ab1dfe319c85a7a6c616a8b1a170631bff6
Carl Guo <carlguo866@gmail.com>
2019-03-14 21:44:29 +0800

    more changes for 美工

---
+++ jiezi/static/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('img').click(function () {
+        video = '<iframe style="width:50vw;height: 27vw; vertical-align: middle" src="' + $(this).attr('data-video') + '"></iframe>';
+        $(this).replaceWith(video);
+    });
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 3000);
+});
