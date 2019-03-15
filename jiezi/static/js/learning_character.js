024a48cde79cbf8157a33a51d805543ca2379367
chenyx <yuxuanchen@stmarksschool.org>
2019-03-15 21:21:13 +0800

    finish audio

---
+++ jiezi/static/js/learning_character.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    var url = "http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text=" + encodeURI( $('.letter').text() );
+    var audio = new Audio(url);
+    audio.src = url;
+    $(".audio").click(function(){
+        audio.play();
+    });
+})
