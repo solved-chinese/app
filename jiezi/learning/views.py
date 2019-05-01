54323b353202a04c35edaa1dc2c5580831d4f213
chenyx512 <yuxuanchen@stmarksschool.org>
2019-05-01 00:44:42 -0400

    Add character display and manage stack backend

--- jiezi/learning/views.py
+++ jiezi/learning/views.py
@@ -1,4 +1,3 @@
-import datetime
 import pandas as pd
 
 from django.shortcuts import render, redirect
@@ -7,26 +6,37 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from learning.models import update_from_df, Character, Radical
 
 
-MIN_LEARN_REVIEW_INTERVAL = 30
-
-
 def index(request):
     return render(request, 'index.html')
+
+
+def display_character(request, character_pk):
+    character = Character.objects.get(pk=character_pk)
+    radicals = [Radical.objects.get(pk=character.radical_1_id)]
+    radicals.append(Radical.objects.get(pk=character.radical_2_id)
+                    if character.radical_2_id else None)
+    radicals.append(Radical.objects.get(pk=character.radical_3_id)
+                    if character.radical_3_id else None)
+    return render(request, 'learning/display_character.html',
+                  {'character':character, 'radicals':radicals})
 
 
 def about_us(request):
     return render(request, 'about_us.html')
 
+
 @login_required()
 def start_learning(request, minutes_to_learn):
     pass
+
 
 @user_passes_test(lambda u: u.is_staff)
 def load_from_excel(request):
     if request.method == 'GET':
         return render(request, 'learning/load_from_excel.html')
-    excel_file = request.FILES['excel_file']
-    radical_df = pd.read_excel(excel_file, 'Radicals')
-    character_df = pd.read_excel(excel_file, 'Characters')
-    response = {'characters':update_from_df(character_df, Character), 'radicals':update_from_df(radical_df, Radical)}
+    excel = request.FILES['excel_file']
+    radical_df = pd.read_excel(excel, 'Radicals')
+    character_df = pd.read_excel(excel, 'Characters')
+    response = {'radicals':update_from_df(radical_df, Radical)}
+    response['characters'] = update_from_df(character_df, Character)
     return render(request, 'learning/load_from_excel.html', response)
