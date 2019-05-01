1ea389633c4208c833e65878751a110cee11ed16
George Yu <yudumpling@gmail.com>
2019-05-01 11:30:20 +0800

    Add new temporary front end files

---
+++ front-end-temp/static/scripts/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#navbar',
+  data: {
+    username: 'George Yu'
+  }
+})
+
+var app = new Vue({
+  el: '#page-container',
+  data: {
+    firstName: 'George',
+    streakDayCount: 3,
+    streakMonth: 'March'
+  }
+})
