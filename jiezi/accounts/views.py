545c63c8ad66a9428c47a58675b808460cedb958
Miclin1024 <miclin1024@foxmail.com>
2019-06-03 22:35:54 +0800

    Fixed bugs and improved responsive

--- jiezi/accounts/views.py
+++ jiezi/accounts/views.py
@@ -33,7 +33,7 @@ def profile(request):
 def manage_library(request):
     return render(request, 'accounts/manage_library.html')
 
-#@login_required
+@login_required
 def dashboard(request):
     active = request.GET.get('active', 'Dashboard')
     return render(request, 'accounts/dashboard.html', {'active': active})
