ed54230e55b1fd8ef180fadcad596f1f6c59fdd3
C T Chen <34198336+the-null-terminator@users.noreply.github.com>
2019-07-11 21:00:59 -0700

    added links

---
+++ views.py
@@ -0,0 +1,15 @@
+"""
+This view serves only the basic website structure like index page
+"""
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+def about_us(request):
+    return render(request, 'about_us.html')
+def the_science_behind(request):
+    return render(request, 'the_science_behind.html')
+def help(request):
+    return render(request, 'help.html')
+
