9e3c6540e7af386d8be304f4d537d77e3d633e6d
George Yu <yudumpling@gmail.com>
2019-08-07 01:01:13 -0400

    Add placeholder add new set modal

--- static/scripts/manage_library.js
+++ static/scripts/manage_library.js
@@ -1,5 +1,10 @@
-function get_available_sets() {
-    $.post('/accounts/get_available_sets/', data => {
-        console.log(data.sets)
+$.post('/accounts/get_available_sets/', data => {
+    data.sets.forEach(set => {
+        $('#available-sets-container').append(`
+            <div class="set-name-container" data-set-id="${set.pk}">
+                <span>${ set.fields.name }</span>
+                <i class="far fa-plus"></i>
+            </div>
+        `);
     });
-}
+});
