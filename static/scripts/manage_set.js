e72003202201a34bce4f23b29dc825b6cbed1c26
George Yu <yudumpling@gmail.com>
2019-08-10 14:47:25 -0400

    Update radical on load

--- static/scripts/manage_set.js
+++ static/scripts/manage_set.js
@@ -1,33 +1,4 @@
-function pad(num, size) {
-    let s = num + '';
-    while (s.length < size) s = '0' + s;
-    return s;
-}
-
-
-const radicalsLookup = {};
-let selectedChar = $($('.chars-container').children()[0]);
-let selectedCharId = selectedChar.attr('data-char-id');
-
-
-selectedChar.addClass('selected');
-$(`.character-card-container[data-char-id=${selectedCharId}]`).parent().show();
-
-$('.char').click(e => {
-    let el = $(e.target);
-    while (!el.attr('class').split(/\s+/).includes('char')) el = el.parent();
-    let chosenCharId = el.attr('data-char-id');
-    if (chosenCharId === selectedCharId) return;
-
-    selectedChar.removeClass('selected');
-    $(`.character-card-container[data-char-id=${selectedCharId}]`).parent().hide();
-
-    selectedChar = el;
-    selectedCharId = chosenCharId;
-    selectedChar.addClass('selected');
-    $(`.character-card-container[data-char-id=${selectedCharId}]`).parent().show();
-
-    // Show radicals info
+function updateRadical() {
     let charRadicalsContainer = $(`.character-card-container[data-char-id=${selectedCharId}] .radicals-breakdown-container`);
     radicalIds = charRadicalsContainer
                  .attr('data-radical-ids')
@@ -48,7 +19,6 @@ $('.char').click(e => {
         } else {
             $.post('/learning/get_radical/', {radical_id: radicalId}, data => {
                 radical = data.radical.fields;
-                console.log(radical);
                 radicalsLookup[radicalId] = radical;
                 charRadicalsContainer.append(`
                 <div class="radical-container">
@@ -58,8 +28,35 @@ $('.char').click(e => {
                     <div class="radical-pictograph" style="background-image: url(/media/${ radical.mnemonic_image })"></div>
                     <p>${ radical.mnemonic_explanation }</p>
                 </div>`);
-            })
+            });
         }
-        console.log(radical);
     });
+}
+
+
+const radicalsLookup = {};
+let selectedChar = $($('.chars-container').children()[0]);
+let selectedCharId = selectedChar.attr('data-char-id');
+
+selectedChar.addClass('selected');
+$(`.character-card-container[data-char-id=${selectedCharId}]`).parent().show();
+
+updateRadical();
+
+
+$('.char').click(e => {
+    let el = $(e.target);
+    while (!el.attr('class').split(/\s+/).includes('char')) el = el.parent();
+    let chosenCharId = el.attr('data-char-id');
+    if (chosenCharId === selectedCharId) return;
+
+    selectedChar.removeClass('selected');
+    $(`.character-card-container[data-char-id=${selectedCharId}]`).parent().hide();
+
+    selectedChar = el;
+    selectedCharId = chosenCharId;
+    selectedChar.addClass('selected');
+    $(`.character-card-container[data-char-id=${selectedCharId}]`).parent().show();
+
+    updateRadical();
 })
