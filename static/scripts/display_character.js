b9d8ddc2d6ca5290cee10bae4ac5da8f3c708825
George Yu <yudumpling@gmail.com>
2019-09-22 20:14:02 +0800

    Fix unadded ID tag

--- static/scripts/display_character.js
+++ static/scripts/display_character.js
@@ -8,18 +8,18 @@ $.get({
         pk: pk
     }
 })
-    .done(data => {
-        if (data.success) {
-            audio = new Howl({
-                src: [`/media/audio/${pk}.mp3`]
-            });
-            $('#speak-button').removeClass('disabled');
-            $('#speak-button').click(() => audio.play());
-        }
-    })
-    .fail((jqXhr, textStatus, errorMessage) => {
-        $('#audio-error-msg').fadeIn();
-        setTimeout(() => {
-            $('#audio-error-msg').fadeOut();
-        }, 3500);
-    });
+.done(data => {
+    if (data.success) {
+        audio = new Howl({
+            src: [`/media/audio/${pk}.mp3`]
+        });
+        $('#speak-button').removeClass('disabled');
+        $('#speak-button').click(() => audio.play());
+    }
+})
+.fail((jqXhr, textStatus, errorMessage) => {
+    $('#audio-error-msg').fadeIn();
+    setTimeout(() => {
+        $('#audio-error-msg').fadeOut();
+    }, 3500);
+});
