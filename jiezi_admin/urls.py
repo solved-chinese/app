d34914ffeba98269bdacee106b14779257f775e6
chenyx512 <yuxuanchen@stmarksschool.org>
2020-09-10 09:58:03 -0400

    Remove color coded image, show media update progress

--- jiezi_admin/urls.py
+++ jiezi_admin/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 
 urlpatterns = [
     path('update_entry', views.update_entry, name='update_entry'),
-    path('update_media', views.update_media, name='update_media'),
-    path('media_update_status', views.media_update_status, name='media_update_status'),
+    path('update_character_animated_stroke_order_image',
+         views.update_character_animated_stroke_order_image,
+         name='update_character_animated_stroke_order_image'),
+    path('update_radical_mnemonic_image',
+         views.update_radical_mnemonic_image,
+         name='update_radical_mnemonic_image'),
+    path('task/<slug:task_id>/', views.task_info, name='task_info'),
+    path('kill_task/', views.kill_task, name='kill_task'),
 ]
