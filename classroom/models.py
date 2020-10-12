6d4b1015da797cd195a1f94965b8114747e62a6b
chenyx <yuxuanchen@stmarksschool.org>
2020-10-12 04:05:13 -0400

    Add Classes

--- classroom/models.py
+++ classroom/models.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 
 from django.db import models
 
@@ -8,7 +9,10 @@ from accounts.models import User
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 primary_key=True, related_name='student')
-
+    in_class = models.ForeignKey('classroom.Class', on_delete=models.SET_NULL,
+                                 related_name='students',
+                                 related_query_name='student',
+                                 null=True, blank=True)
     study_streak = models.IntegerField(default=0)
     last_study_time = models.DateTimeField(auto_now_add=True)
     last_study_duration = models.DurationField(
@@ -27,3 +31,10 @@ class Student(models.Model):
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 primary_key=True, related_name='teacher')
+
+
+class Class(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,
+                                related_name='classes',
+                                related_query_name='class')
+    uuid = models.UUIDField(default=uuid.uuid4)
