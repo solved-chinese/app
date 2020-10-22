54bcada07df182a87bd5b081d5ac271b260b397c
chenyx512 <yuxuanchen@stmarksschool.org>
2020-10-22 11:12:28 +0800

    Add about_us test

--- jiezi/tests/test_index.py
+++ jiezi/tests/test_index.py
@@ -9,16 +9,23 @@ class TestIndex(TestCase):
         response = self.client.get(reverse('index'))
         self.assertTemplateUsed(response, 'unauthenticated_index.html')
         self.assertEqual(response.status_code, 200)
+        self.test_about()
 
     def test_teacher(self):
         teacher = create_teacher()
         self.client.force_login(teacher.user)
         response = self.client.get(reverse('index'))
         self.assertRedirects(response, reverse('list_class'))
+        self.test_about()
 
     def test_student(self):
         student = create_student()
         self.client.force_login(student.user)
         response = self.client.get(reverse('index'))
         self.assertTemplateUsed(response, 'student_index.html')
+        self.assertEqual(response.status_code, 200)
+        self.test_about()
+
+    def test_about(self):
+        response = self.client.get('about_us')
         self.assertEqual(response.status_code, 200)
