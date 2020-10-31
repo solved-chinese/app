2384933052bc551324917f1d017249a886dab77c
Tianyue <Cheng>
2020-11-01 00:33:26 +0800

    Update Indent

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -12,8 +12,9 @@ $.ajaxSetup({
             console.log('add csrf');
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
-    },
+    }
 });
+
 
 /***********************************
   Modal Helper Functions
@@ -29,6 +30,7 @@ function hideModal(modalId) {
     $(`#${modalId}`).hide();
 }
 
+
 /***********************************
   Search
 ***********************************/
@@ -37,9 +39,9 @@ const MAX_ENTRIES_DISPLAY = 6;
 const $source = $('.search-input-wrapper > input');
 const $target = $('#search-dropdown-wrapper');
 
-$('.search-form-wrapper').focusout(() =>
+$('.search-form-wrapper').focusout(() => {
     setTimeout(() => {
-        $target.html("");
+        $target.html('');
     }, 150);
 });
 
@@ -56,35 +58,35 @@ function searchHandler(e) {
             keyword: query
         }
     })
-        .done(data => {
-            $target.empty();
-            $('<hr>').appendTo($target);
+    .done(data => {
+        $target.empty();
+        $('<hr>').appendTo($target);
 
-            if (data.length === 0) {
-                $('<div class="error-msg">No Match</div>').appendTo($target);
-                return;
-            }
-
-            for (i = 0; i < Math.min(data.length, MAX_ENTRIES_DISPLAY); i++) {
-                char = data[i];
-                let targetPk = char.id.toString().padStart(4, '0');
-                let entry = `
-                <a href='/content/C${targetPk}' class='search-entry-wrapper'>
-                    <div class='search-entry'>
-                        <span class='character'>${char.chinese}</span>
-                        <span class='pinyin'>${char.pinyin.replace(/\s+/g, '')}</span>
-                        <p class='definition'>${char.definition_1}
-                        </p>
-                    </div>
-                </a>`;
-                // Some pinyins contain whitespaces, hence they are removed here (temporary fix)
-                $(entry).appendTo($target);
-            }
-        })
-        .fail((xhr, status, errorThrown) => {
-            $target.html('<hr><div class="error-msg">There was a problem connecting with the Solved server</div>');
+        if (data.length === 0) {
+            $('<div class="error-msg">No Match</div>').appendTo($target);
             return;
-        });
+        }
+
+        for (i = 0; i < Math.min(data.length, MAX_ENTRIES_DISPLAY); i++) {
+            char = data[i];
+            let targetPk = char.id.toString().padStart(4, '0');
+            let entry = `
+            <a href='/content/C${targetPk}' class='search-entry-wrapper'>
+                <div class='search-entry'>
+                    <span class='character'>${char.chinese}</span>
+                    <span class='pinyin'>${char.pinyin.replace(/\s+/g, '')}</span>
+                    <p class='definition'>${char.definition_1}
+                    </p>
+                </div>
+            </a>`;
+            // Some pinyins contain whitespaces, hence they are removed here (temporary fix)
+            $(entry).appendTo($target);
+        }
+    })
+    .fail((xhr, status, errorThrown) => {
+        $target.html('<hr><div class="error-msg">There was a problem connecting with the Solved server</div>');
+        return;
+    });
 }
 
 $source.on("input propertychange", searchHandler);
