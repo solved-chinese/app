f9c7124a3b0ef0ef6ac191cdf2f79d5050853da2
Tianyue <Cheng>
2020-10-31 23:06:30 +0800

    Added Responsiveness
    Added responsiveness according to latest design

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -3,94 +3,118 @@
 ***********************************/
 var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
 function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	// these HTTP methods do not require CSRF protection
+	return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
 }
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            console.log('add csrf');
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
+	beforeSend: function (xhr, settings) {
+		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			console.log("add csrf");
+			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		}
+	},
 });
-
 
 /***********************************
   Modal Helper Functions
 ***********************************/
 
 function showModal(modalId) {
-    $('.page-mask').show();
-    $(`#${modalId}`).show();
+	$(".page-mask").show();
+	$(`#${modalId}`).show();
 }
 
 function hideModal(modalId) {
-    $('.page-mask').hide();
-    $(`#${modalId}`).hide();
+	$(".page-mask").hide();
+	$(`#${modalId}`).hide();
 }
-
 
 /***********************************
   Search
 ***********************************/
 
 const MAX_ENTRIES_DISPLAY = 6;
-const $source = $('.search-input-wrapper > input');
-const $target = $('#search-dropdown-wrapper');
+const $source = $(".search-input-wrapper > input");
+const $target = $("#search-dropdown-wrapper");
 
-$('.search-form-wrapper').focusout(() => {
-    setTimeout(() => {
-        $target.html('');
-    }, 150);
+$(".search-form-wrapper").focusout(() => {
+	setTimeout(() => {
+		$target.html("");
+	}, 150);
 });
 
 function searchHandler(e) {
-    let query = $source.val();
-    if (query === '') {
-        $target.html('');
-        return;
-    }
+	let query = $source.val();
+	if (query === "") {
+		$target.html("");
+		return;
+	}
 
-    $.post({
-        url: '/content/search/',
-        data: {
-            keyword: query
-        }
-    })
-    .done(data => {
-        $target.empty();
-        $('<hr>').appendTo($target);
+	$.post({
+		url: "/content/search/",
+		data: {
+			keyword: query,
+		},
+	})
+		.done((data) => {
+			$target.empty();
+			$("<hr>").appendTo($target);
 
-        if (data.length === 0) {
-            $('<div class="error-msg">No Match</div>').appendTo($target);
-            return;
-        }
+			if (data.length === 0) {
+				$('<div class="error-msg">No Match</div>').appendTo($target);
+				return;
+			}
 
-        for (i = 0; i < Math.min(data.length, MAX_ENTRIES_DISPLAY); i++) {
-            char = data[i];
-            let targetPk = char.id.toString().padStart(4, '0');
-            let entry = `
+			for (i = 0; i < Math.min(data.length, MAX_ENTRIES_DISPLAY); i++) {
+				char = data[i];
+				let targetPk = char.id.toString().padStart(4, "0");
+				let entry = `
                 <a href='/content/C${targetPk}' class='search-entry-wrapper'>
                     <div class='search-entry'>
                         <span class='character'>${char.chinese}</span>
-                        <span class='pinyin'>${char.pinyin.replace(/\s+/g, '')}</span>
+                        <span class='pinyin'>${char.pinyin.replace(
+							/\s+/g,
+							""
+						)}</span>
                         <p class='definition'>${char.definition_1}
                         </p>
                     </div>
                 </a>`;
-            // Some pinyins contain whitespaces, hence they are removed here (temporary fix)
-            $(entry).appendTo($target);
-        }
-    })
-    .fail((xhr, status, errorThrown) => {
-        $target.html('<hr><div class="error-msg">There was a problem connecting with the Solved server</div>');
-        return;
-    });
+				// Some pinyins contain whitespaces, hence they are removed here (temporary fix)
+				$(entry).appendTo($target);
+			}
+		})
+		.fail((xhr, status, errorThrown) => {
+			$target.html(
+				'<hr><div class="error-msg">There was a problem connecting with the Solved server</div>'
+			);
+			return;
+		});
 }
 
-$source.on('input propertychange', searchHandler);
-$source.keypress(e => {
-    if (e.keyCode !== 13 || $('.search-entry-wrapper').length === 0) return;
-    $('.search-entry-wrapper')[0].click();
+$source.on("input propertychange", searchHandler);
+$source.keypress((e) => {
+	if (e.keyCode !== 13 || $(".search-entry-wrapper").length === 0) return;
+	$(".search-entry-wrapper")[0].click();
+});
+
+/***********************************
+  Responsive
+***********************************/
+
+$(function () {
+	var winWide =
+		window.innerWidth ||
+		document.documentElement.clientWidth ||
+		document.body.clientWidth;
+	var btn = document.getElementById("userDropdown");
+	var btn_mobile = document.getElementById("userDropdown-mobile");
+
+	if (winWide <= 768) {
+		btn.style.display = "none";
+		btn_mobile.style.display = "block";
+	} else {
+		btn.style.display = "block";
+		btn_mobile.style.display = "none";
+	}
 });
