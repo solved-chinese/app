88afc42d784c24886c9316e0e442bab47c9272e6
chenyx512 <yuxuanchen@stmarksschool.org>
2020-11-05 22:40:39 +0800

    Remove guest access from unrelated things

--- jiezi/views.py
+++ jiezi/views.py
@@ -6,12 +6,17 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.reverse import reverse as rest_reverse
+from django.contrib.auth import logout
 
 from learning.models import LearningProcess, StudentCharacter, StudentCharacterTag
 
 
 def index(request):
     if not request.user.is_authenticated:
+        return render(request, 'unauthenticated_index.html')
+    elif request.user.is_guest:
+        request.user.delete()
+        logout(request)
         return render(request, 'unauthenticated_index.html')
     elif request.user.is_student:
         student = request.user.student
