8c40dd3458bd748d8597c8b18ce21727bb4c6cea
chenyx512 <yuxuanchen@stmarksschool.org>
2020-11-21 13:51:33 +0800

    Enable viewing of related characters to a radical in admin

--- content/admin.py
+++ content/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Character, Radical, CharacterSet
+from django.utils.html import format_html
+from django.urls import reverse
 
 
 class RadicalAdmin(admin.ModelAdmin):
     search_fields = ['chinese', 'id']
+    list_display = ['__str__', 'get_characters_list_display']
+
+    def get_characters_list_display(self, radical):
+        s = ""
+        for c in radical.characters.all():
+            s += f"<a href={reverse('display_character', args=[c.pk])}>" \
+                 f"{c.chinese}</a>, "
+        return format_html(s)
 
 
 admin.site.register(Radical, RadicalAdmin)
