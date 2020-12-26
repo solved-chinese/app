86d2a658dcb8e4729c6179ad4aef527b3b2da77f
YaleChen299 <proverb299@gmail.com>
2020-12-27 01:33:03 +0800

    Added new component WordDefinition

--- learning/src/components/App.js
+++ learning/src/components/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import WordDefinition from "./WordDefinition";
 
 class App extends Component {
     render() {
-        return "Hello World";
+        //change the following constants with actual data
+        const cn = "字";
+        const py = "zi";
+        const src = "here";
+        const arr = [
+            {
+                "part_of_speech": "n.",
+                "definition"    : "a noun \n second line"
+            },
+            {
+                "part_of_speech": "adj.",
+                "definition"    : "an adj"
+            }];
+
+        return (
+            <div>
+                <WordDefinition
+                    word={cn}
+                    pinin={py}
+                    audiosrc={src}
+                    definitions={arr}
+                />
+            </div>
+        );
     }
 }
 
