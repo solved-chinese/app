342643b00df369b41b870eaf41b76cae8a487527
Yuxuan Chen <chenyx0512@gmail.com>
2021-01-20 18:05:41 +0800

    Lower potential num choices in MC

--- content/question_factories/constants.py
+++ content/question_factories/constants.py
@@ -8,6 +8,6 @@ class QuestionExists(CannotAutoGenerate):
         super().__init__(msg, *args, **kwargs)
 
 
-MAX_MC_CHOICE_NUM = 10
-MIN_MC_CHOICE_NUM = 5
+MAX_MC_CHOICE_NUM = 5
+MIN_MC_CHOICE_NUM = 3
 MAX_RANDOM_CHOICE_NUM = 20
