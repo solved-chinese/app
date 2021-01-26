98cd236a6dff53512a9c46f7be8f1c3b38e60640
Yuxuan Chen <chenyx0512@gmail.com>
2021-01-26 21:01:10 +0800

    Debug reset_content

--- content/management/commands/reset_content.py
+++ content/management/commands/reset_content.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.core.management import call_command
+from content.models import AudioFile
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        app_models = apps.get_app_config('content').get_models()
+        app_models = list(apps.get_app_config('content').get_models())
+        app_models.remove(AudioFile)
+        app_models.append(AudioFile)
+
         print("now delete all content objects")
         for model in app_models:
             print(model.objects.all().delete())
