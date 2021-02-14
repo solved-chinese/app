6e9f6aed76cafa16a41a14e4744ad982d5688486
Yuxuan Chen <chenyx0512@gmail.com>
2021-02-14 17:32:52 +0800

    Better accounts and clasroom admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('display_name', 'username', 'email', 'last_login',
-                    'is_staff')
-    list_filter = ['is_staff']
-    search_fields = ['display_name', 'username', 'email']
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('display_name', 'email')}),
+        ('Solved specific info', {'fields': ('is_student', 'is_teacher')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser',
+                       'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'display_name', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',
+                   'is_student', 'is_teacher')
+    search_fields = ('username', 'display_name', 'email')
--- classroom/admin.py
+++ classroom/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
-from .models import Student, Teacher, Class, Assignment
+from django.utils.html import format_html
+
+from .models import Class, Assignment
 
 
-@admin.register(Student, Teacher, Class, Assignment)
-class ClassroomAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('detail', 'class_name', 'teacher_name')
+    list_display_links = None
+
+    def class_name(self, obj):
+        return format_html('<a href={}>{}</a>',
+                           obj.klass.get_absolute_url(),
+                           obj.klass.name)
+    class_name.short_description = 'class'
+
+    def teacher_name(self, obj):
+        return obj.in_class.teacher.display_name
+    teacher_name.short_description = 'teacher'
+
+    def detail(self, obj):
+        return format_html('<a href={}>{}</a>',
+                           obj.get_absolute_url(),
+                           obj.name)
+    detail.short_description = 'name'
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('detail', 'teacher_display_name', 'student_count')
+    list_display_links = None
+
+    def teacher_display_name(self, obj):
+        return obj.teacher.display_name
+    teacher_display_name.short_description = 'teacher'
+
+    def detail(self, obj):
+        return format_html('<a href={}>{}</a>',
+                           obj.get_absolute_url(),
+                           obj.name)
+    detail.short_description = 'name'
