78498d0c92257c13be07ac5b3dbb75308b7f4619
Yuxuan Chen <chenyx0512@gmail.com>
2021-02-17 17:10:19 +0800

    Add back quick creation of WordSet

--- content/forms.py
+++ content/forms.py
@@ -6,40 +6,35 @@ from .models import WordSet, Word, LinkedField, AudioFile
 from .utils import punctuate_English, punctuate_Chinese, add_highlight
 
 
-# class WordSetQuickCreateFrom(forms.ModelForm):
-#     words = forms.CharField(
-#         widget=forms.Textarea,
-#         help_text="input each word in a new line"
-#     )
-#
-#     def clean_words(self):
-#         words = self.cleaned_data['words']
-#         words = words.split('\n')
-#         cleaned_words = []
-#         for word in words:
-#             word = word.strip()
-#             if not word:
-#                 continue
-#             word_objects = Word.objects.filter(chinese=word)
-#             cnt = word_objects.count()
-#             if cnt == 1:
-#                 word_object = word_objects.get()
-#             elif cnt > 1:
-#                 word_object = Word.get_TODO_word()
-#             else:
-#                 word_object = Word.objects.get_or_create(
-#                     chinese=word)[0]
-#             cleaned_words.append(word_object)
-#         return cleaned_words
-#
-#     class Meta:
-#         model = WordSet
-#         fields = ['name', 'words']
-
-
 class WordSetCreationForm(forms.ModelForm):
+    words = forms.CharField(
+        widget=forms.Textarea,
+        help_text="input each word in a new line"
+    )
+
+    def clean_words(self):
+        words = self.cleaned_data['words']
+        words = words.split('\n')
+        cleaned_words = []
+        for word in words:
+            word = word.strip()
+            if not word:
+                continue
+            word_objects = Word.objects.filter(chinese=word)
+            cnt = word_objects.count()
+            if cnt == 1:
+                word_object = word_objects.get()
+            elif cnt > 1:
+                word_object = Word.get_TODO_word()
+            else:
+                word_object = Word.objects.get_or_create(
+                    chinese=word)[0]
+            cleaned_words.append(word_object)
+        return cleaned_words
+
     class Meta:
-        fields = ['name', 'jiezi_id', 'parent']
+        model = WordSet
+        fields = ['name', 'jiezi_id', 'parent', 'words']
 
 
 class LinkedFieldModelSelect2(WidgetMixin,
