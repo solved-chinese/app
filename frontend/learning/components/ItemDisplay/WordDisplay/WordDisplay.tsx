2ae2fd6514feb9820607343d0bc1aca2bdc6c5c1
Michael Lin <miclin@berkeley.edu>
2021-04-05 15:55:29 -0700

    Done with word display

--- frontend/learning/components/ItemDisplay/WordDisplay/WordDisplay.tsx
+++ frontend/learning/components/ItemDisplay/WordDisplay/WordDisplay.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import WordDefinition from './WordDefinition';
@@ -7,7 +6,7 @@ import ExampleSentences from './ExampleSentences';
 
 import BreakdownView from '@learning.components/ItemDisplay/BreakdownView';
 import LoadingView from '@learning.components/ItemDisplay/LoadingView';
-import useLoadWord from '@learning.hooks/useLoadWord.ts';
+import useLoadWord from '@learning.hooks/useLoadWord';
 
 import { Word } from '@interfaces/CoreItem';
 
@@ -38,49 +37,66 @@ const ExampleSentenceHeading = styled.h2`
     text-align: center;
 `;
 
+type Props = {
+    /**
+     * The word object to be rendered, if not provided,
+     * url will be used to construct the object.
+     */
+    word?: Word,
+
+    /**
+     * The URL of the word to be rendered, if it
+     * is not provided, then the qid is used to construct
+     * the url.
+     */
+    url?: string,
+
+    /**
+     * The query id of the word to be rendered, will
+     * be omitted if url is present and not null.
+     */
+    qid?: number,
+
+    /* Determine whether the breakdown view is always expanded. */
+    autoExpandBreakdown?: boolean,
+}
+
 /**
  * The main function that renders a word view.
- * @param { Word } props.word The word object to be rendered, if not provided,
- * the url param will be used to fetch the object.
- * @param { string } props.url The URL of the word to be rendered, if
- * not provided, the qid is used to construct the url.
- * @param { number } props.qid The query id of the word to be rendered, will
- * be omitted if either url or word is not null.
- * @param { boolean } props.autoExpandBreakdown Determine whether the breakdown
- * view is always expanded.
- * @returns { JSX.Element }
  */
-export default function WordDisplay(props) {
+const WordDisplay = (props: Props): JSX.Element => {
 
-    const word = props.word == null ?
+    const word: Word | null = props.word == null ?
         useLoadWord(props.url == null ?
             `/content/word/${props.qid}` : props.url
         ) : props.word;
 
-    const renderBreakdown = () => {
+    const renderBreakdown = (word: Word): JSX.Element => {
         // if it is single-character word, the characters field will be a list of
         // a single serialized character, render that character's breakdown view
-        if (word.characters.length === 1 && word.characters[0].radicals != null)
-            return(
+        if (word.characters.length === 1) {
+            const radicals = word.characters[0].split(';');
+            return (
                 <BreakdownView
                     type='radical'
-                    alwaysDisplay={props.autoExpandBreakdown}
-                    componentURL={word.characters[0].radicals}
+                    alwaysDisplay={props.autoExpandBreakdown ?? true}
+                    componentURL={radicals}
                     memoryAid={word.memoryAid}
                 />
             );
-        else
-            return(
+        } else {
+            return (
                 <BreakdownView
                     type='word'
-                    alwaysDisplay={props.autoExpandBreakdown}
+                    alwaysDisplay={props.autoExpandBreakdown ?? true}
                     componentURL={word.characters}
                     memoryAid={word.memoryAid}
                 />
             );
+        }
     };
 
-    const renderWord = (word) => {
+    const renderWord = (word: Word): JSX.Element => {
         const chinese = word.chinese;
         const pinyin = word.pinyin;
         const definitions = word.definitions;
@@ -98,7 +114,7 @@ export default function WordDisplay(props) {
                     />
                 </ContainerTop>
 
-                { renderBreakdown() }
+                { renderBreakdown(word) }
                 <br/>
 
                 <ExampleSentenceHeading>
@@ -106,7 +122,7 @@ export default function WordDisplay(props) {
                 </ExampleSentenceHeading>
                 
                 <ContainerBottom>
-                    {word.sentences.map((sen, i) => {
+                    {word.sentences.map((sen) => {
                         return (
                             <ExampleSentences
                                 key={sen.chineseHighlight}
@@ -128,32 +144,6 @@ export default function WordDisplay(props) {
     } else {
         return renderWord(word);
     }
-}
-
-WordDisplay.propTypes = {
-    /**
-     * The word object to be rendered, if not provided,
-     * url will be used to construct the object.
-     */
-    word: PropTypes.object,
-
-    /**
-     * The URL of the word to be rendered, if it
-     * is not provided, then the qid is used to construct
-     * the url.
-     */
-    url: PropTypes.string,
-
-    /**
-     * The query id of the word to be rendered, will
-     * be omitted if url is present and not null.
-     */
-    qid: PropTypes.number,
-
-    /* Whether the breakdown view is always expanded. */
-    autoExpandBreakdown: PropTypes.bool,
 };
 
-WordDisplay.defaultProps = {
-    autoExpandBreakdown: true,
-};
+export default WordDisplay;
