3f95c6e9cc22265f011090e1c77a1efd9a87b52b
Michael Lin <miclin@berkeley.edu>
2021-04-09 14:50:53 -0700

    Core learning docs

--- frontend/interfaces/CoreLearning.ts
+++ frontend/interfaces/CoreLearning.ts
@@ -18,28 +18,56 @@ export interface ProgressBarData {
     bonus: number,
 }
 
-/**
- * There are four action kinds for a learning step. The specific learning
- * instructions are returned by server upon calling service/getLearningNext.
- * If *action = review*, it means react should render a review question.
- * A *display* means displaying an word/character/radical object, and
- * *assignment* is for viewing the active assignment.
- */
 export type LearningAction = 'review' | 'display' | 'assignment' | 'done'
-
 
 export type LearningObjectContent = DisplayObjectContent | ReviewQuestionData
 
-export interface LearningObjectDescriptor {
+/**
+ * An instance that describes a learning step.
+ */
+export interface LearningStepDescriptor {
+
+    /**
+     * The **action type** for a learning step.
+     *
+     * There are four action kinds for a learning step. The specific learning
+     * instructions are returned by server upon calling service/getLearningNext.
+     * If `action = review`, it means react should render a review question.
+     * A `display` instruction means displaying an word/character/radical object, and
+     * `assignment` is for viewing the active assignment.
+     */
     action: LearningAction,
+
+    /**
+     * The **content payload** of the learning action.
+     *
+     * If `action = review`, the content
+     * will be of type `ReviewQuestionData`, else if `action = display`, the content
+     * would have type `DisplayObjectContent`. Content is undefined for assignment and
+     * done instructions.
+     */
     content: LearningObjectContent
 }
 
-export interface LearningNextResponse extends LearningObjectDescriptor {
+/**
+ * The server response when calling getLearningNext.
+ */
+export interface LearningNextResponse extends LearningStepDescriptor {
+    /**
+     * Data for rendering the progress bar in CoreLearning
+     */
     progressBar: ProgressBarData,
+
+    /**
+     * State identifier in a learning step, used for server
+     * status verification.
+     */
     state: string,
 }
 
+/**
+ * The server response when submitting an answer.
+ */
 export interface AnswerSubmitResponse extends LearningNextResponse {
     isCorrect: boolean,
     answer: ReviewQuestionAnswer
