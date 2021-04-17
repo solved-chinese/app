d368c93ee36a81dd8dda59069d1ad17f4a747da9
Yuxuan Chen <chenyx0512@gmail.com>
2021-04-17 17:51:02 +0800

    Fixes wordset cleaning problems with general questions is_done filter
    fixes SOLVEDCHINESE-3N

--- content/models/word_set.py
+++ content/models/word_set.py
@@ -41,8 +41,7 @@ class WordSet(MPTTModel, GeneralContentModel):
                 for w in self.words.all():
                     if not w.is_done:
                         raise ValidationError(f"{repr(w)} not done")
-                    elif not w.get_reviewable_object().questions.filter(
-                            is_done=True).exists():
+                    elif not w.get_reviewable_object().questions.filter().exists():
                         raise ValidationError(f"{repr(w)} has no questions")
             else:
                 if self.words.exists():
